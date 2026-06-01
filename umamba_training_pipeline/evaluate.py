54df79b7e20190ce5e90fe6efe691cb6b1ffb3a4
xinyizhiren <3081014477@qq.com>
2026-06-01 15:46:05 +0800

    result

--- umamba_training_pipeline/evaluate.py
+++ umamba_training_pipeline/evaluate.py
@@ -40,7 +40,7 @@ def parse_args():
     parser.add_argument("--model-name", choices=["umamba", "autophasenn", "autophasenn_relu"], default="umamba")
     parser.add_argument("--phase-activation", choices=["tanh", "atan"], default="tanh")
     parser.add_argument("--phase-logit-scale", type=float, default=1.0)
-    parser.add_argument("--checkpoint", default="/data_ssd/oyys/autophasenn/Unsupfalse_Dfalse_Ufalse_T0.1_comb2_batch8_plateau_Init1e-3_adam_scale1/best_model.pt")
+    parser.add_argument("--checkpoint", default="/home/oyys/code/UMamba-AutoPhaseNN/umamba_training_pipeline/output/checkpoint.pt")
     parser.add_argument("--data-dir", default="/data_ssd/oyys/autophasenn/")
     parser.add_argument("--data-diff", default="val_diff.npy")
     parser.add_argument("--data-real", default="val_real.npy")
